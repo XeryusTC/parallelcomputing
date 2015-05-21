a58254d4bf4a9a598ba8c50c2bfce57706e23665
XeryusTC <armadillo@onenetbeyond.org>
2015-05-21 23:04:40 +0200

    Complete code for mandelbrot exercise

--- MPI/mandelbrot/mandelbrot2.c
+++ MPI/mandelbrot/mandelbrot2.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
+#include <string.h>
 
 #define WIDTH 4096
 #define HEIGHT 3072
@@ -107,38 +108,35 @@ static void writePPM(Image im, char *filename)
 }
 
 static void mandelbrotSet(double centerX, double centerY,
-        double scale, Image image)
+        double scale, Image image, int row)
 { /* routine that computes an image of the mandelbrot set */
     int w=image->width, h=image->height, **im=image->imdata;
     double a, b;
     double x, y, z;
-    int i, j, k;
+    int j, k;
 
-    for (i = (rank*interval); i < (rank+1)*interval; i++)
+    b = centerY + row*scale - ((h/2)*scale);
+    for (j = 0; j < w; j++)
     {
-        b = centerY + i*scale - ((h/2)*scale);
-        for (j = 0; j < w; j++)
+        a = centerX + j*scale - ((w/2)*scale);
+        x = a;
+        y = b;
+        k = 0;
+        while ((x*x + y*y <= 100) && (k < MAXITER))
         {
-            a = centerX + j*scale - ((w/2)*scale);
-            x = a;
-            y = b;
-            k = 0;
-            while ((x*x + y*y <= 100) && (k < MAXITER))
-            {
-                z = x;
-                x = x*x - y*y + a;
-                y = 2*z*y + b;
-                k++;
-            };
-            im[i][j] = k;
-        }
+            z = x;
+            x = x*x - y*y + a;
+            y = 2*z*y + b;
+            k++;
+        };
+        im[row][j] = k;
     }
 }
 
 int main(int argc, char **argv)
 {
     Image mandelbrot;
-    int i;
+    int i, running, data[WIDTH];
     MPI_Status status;
     double t;
 
@@ -151,24 +149,51 @@ int main(int argc, char **argv)
 
     interval = mandelbrot->height / size;
     start = rank * interval;
+    running = size - 1;
 
-    t = MPI_Wtime();
-    mandelbrotSet (-0.65, 0, 2.5/HEIGHT, mandelbrot);
-    printf("process %d: %f\n", rank, MPI_Wtime()-t);
 
     /* reconstruct image */
+    t = MPI_Wtime();
     if (rank == 0) {
-        for (i=start+interval; i<mandelbrot->height; ++i) {
-            MPI_Recv(mandelbrot->imdata[i], mandelbrot->width, MPI_INT,
-                    MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &status);
+        i = 0;
+        while (running > 0) {
+            MPI_Recv(data, mandelbrot->width, MPI_INT, MPI_ANY_SOURCE,
+                    MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            /* slave is returning calculated data, add it to the image */
+            if (status.MPI_TAG < mandelbrot->height) {
+                memcpy(mandelbrot->imdata[status.MPI_TAG], data,
+                        sizeof(int) * mandelbrot->width);
+            }
+
+            /* we are done doing calculations, send termination signal */
+            if (i >= mandelbrot->height) {
+                MPI_Send(&i, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+                running--;
+                printf("Slave %d shut down\n", status.MPI_SOURCE);
+            /* send new data to calculate otherwise */
+            } else {
+                MPI_Send(&i, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+                i++;
+            }
+
         }
+
         writePPM (mandelbrot, "mandelbrot.ppm");
     } else {
-        for (i=start; i<start+interval; ++i) {
+        /* register with the master */
+        MPI_Send(mandelbrot->imdata[0], mandelbrot->width, MPI_INT, 0,
+                mandelbrot->height, MPI_COMM_WORLD);
+        while (1) {
+            MPI_Recv(&i, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            /* check if we received a kill signal */
+            if (status.MPI_TAG == 0)
+                break;
+            mandelbrotSet (-0.65, 0, 2.5/HEIGHT, mandelbrot, i);
             MPI_Send(mandelbrot->imdata[i], mandelbrot->width, MPI_INT, 0, i,
                     MPI_COMM_WORLD);
         }
     }
+    printf("process %d: %f\n", rank, MPI_Wtime()-t);
 
     freeImage(mandelbrot);
 
